4086b9e5f7c645f3cc3cd13155638024c22011e2
eirinhi <eirinhi@stud.ntnu.no>
2026-02-17 21:39:58 +0100

    feat: Impement Purchase class and tests
    - Implemented the Purchase class extending Transaction, representing a purchase transaction in the game
    - Added a committed field to track whether the purchase has been committed (left a TODO to be implemented when Player class is implemented)
    - Added a PurchaseTest class to test the functionality of the Transaction and Purchase classes
    
    issue #10

---
+++ src/main/java/no/ntnu/idatt2003/core/Purchase.java
@@ -0,0 +1,41 @@
+package no.ntnu.idatt2003.core;
+
+/**
+ * Represents a purchase transaction in the stock game.
+ * A purchase transaction involves buying a share of a stock.
+ */
+public class Purchase extends Transaction {
+
+    /**
+     * Creates a new purchase transaction with the specified share and week.
+     *
+     * @param share the share being purchased
+     * @param week the week in which the purchase occurs
+      * @throws IllegalArgumentException if share is null or if week is negative
+     */
+    public Purchase(final Share share, final int week) {
+        super(share, week, new PurchaseCalculator(share));
+    }
+
+    /**
+     * Commits the purchase transaction for the given player.
+     *
+     * @param player the player making the purchase
+     * @throws IllegalStateException if the transaction has been committed
+     * @throws IllegalArgumentException if player is null
+     */
+    @Override
+    public void commit(final Player player) {
+        if (isCommitted()) {
+            throw new IllegalStateException("Transaction already committed");
+        }
+
+        if (player == null) {
+            throw new IllegalArgumentException("Player cannot be null");
+        }
+
+        // TODO: implement when Player is ready
+
+        setCommitted(true);
+    }
+}
