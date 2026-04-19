c83481aa2a30a18bc51ac0472e81d2849a06f858
eirinhi <eirinhi@stud.ntnu.no>
2026-04-19 21:26:56 +0200

    fix: fix pom and App.java
    - fixed pom.xml to use the correct package for App.java
    - updated App.java to set MainLayout as the scene

--- src/main/java/no/ntnu/idatt2003/controller/App.java
+++ src/main/java/no/ntnu/idatt2003/controller/App.java
@@ -1,17 +1,19 @@
 package no.ntnu.idatt2003.controller;
 
+import java.math.BigDecimal;
 import javafx.application.Application;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
 import javafx.stage.Stage;
+import no.ntnu.idatt2003.model.entity.Player;
+import no.ntnu.idatt2003.view.MainLayout;
 
 public class App extends Application {
 
     @Override
     public void start(Stage primaryStage) {
-
-        Label label = new Label("Velkommen til Millions!");
-        Scene scene = new Scene(label, 300, 200);
+        Player player = new Player("Testspiller", new BigDecimal("10000"));
+        MainLayout mainLayout = new MainLayout(player);
+        Scene scene = new Scene(mainLayout, 900, 600);
 
         primaryStage.setTitle("Millions");
         primaryStage.setScene(scene);
